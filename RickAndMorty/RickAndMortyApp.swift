541867de7cc2d53ba00f0fdca3f777a8fd2694a9
Alexander Senyuk <a.senyuk314@gmail.com>
2022-08-20 17:37:48 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Александр Сенюк on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

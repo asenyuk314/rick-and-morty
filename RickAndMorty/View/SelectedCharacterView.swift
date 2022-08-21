a14b85eabb60eb507a6b2e711adeb211ea510c7d
Alexander Senyuk <a.senyuk314@gmail.com>
2022-08-21 23:06:40 +0300

    added animated autoscroll on selectedCharacter change

--- RickAndMorty/View/SelectedCharacterView.swift
+++ RickAndMorty/View/SelectedCharacterView.swift
@@ -13,70 +13,76 @@ struct SelectedCharacterView: View {
   @StateObject var alsoFromVM = AlsoFromViewModel()
 
   var body: some View {
-    List {
-      HStack(alignment: .top) {
-        KFImage(selectedCharacter.imageURL)
-          .resizable()
-          .placeholder({
-            ProgressView()
-          })
-          .aspectRatio(contentMode: .fit)
-          .frame(width: 125, height: 125)
-          .cornerRadius(10)
-        VStack(alignment: .leading, spacing: 0) {
-          Text("Last known location:")
-            .foregroundColor(Color("OrangeColor"))
-            .font(.headline)
-          Text(selectedCharacter.location.name)
-            .foregroundColor(.secondary)
-            .font(.subheadline)
-          if selectedCharacter.firstSeenEpisode.count > 0 {
-            Text("First seen in:")
+    ScrollViewReader { proxy in
+      List {
+        HStack(alignment: .top) {
+          KFImage(selectedCharacter.imageURL)
+            .resizable()
+            .placeholder({
+              ProgressView()
+            })
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 125, height: 125)
+            .cornerRadius(10)
+          VStack(alignment: .leading, spacing: 0) {
+            Text("Last known location:")
               .foregroundColor(Color("OrangeColor"))
               .font(.headline)
-            Text(selectedCharacter.firstSeenEpisode)
+            Text(selectedCharacter.location.name)
               .foregroundColor(.secondary)
               .font(.subheadline)
-          }
-          Text("Status:")
-            .foregroundColor(Color("OrangeColor"))
-            .font(.headline)
-          HStack {
-            Circle()
-              .foregroundColor(K.statusesColors[selectedCharacter.status])
-              .frame(width: 10, height: 10)
-            Text(selectedCharacter.status.rawValue)
-              .foregroundColor(.secondary)
-              .font(.subheadline)
-          }
-        }
-      }
-      .listRowSeparator(.hidden)
-      if alsoFromVM.characters.count > 0 {
-        Section {
-          ForEach(alsoFromVM.characters, id: \.id) { item in
-            Button {
-              swapSelectedCharacter(with: item)
-            } label: {
-              CharacterCardView(character: item)
+            if selectedCharacter.firstSeenEpisode.count > 0 {
+              Text("First seen in:")
+                .foregroundColor(Color("OrangeColor"))
+                .font(.headline)
+              Text(selectedCharacter.firstSeenEpisode)
+                .foregroundColor(.secondary)
+                .font(.subheadline)
+            }
+            Text("Status:")
+              .foregroundColor(Color("OrangeColor"))
+              .font(.headline)
+            HStack {
+              Circle()
+                .foregroundColor(K.statusesColors[selectedCharacter.status])
+                .frame(width: 10, height: 10)
+              Text(selectedCharacter.status.rawValue)
+                .foregroundColor(.secondary)
+                .font(.subheadline)
             }
           }
-        } header: {
-          Text("Also from \"\(selectedCharacter.location.name)\"")
-            .font(.title)
-            .bold()
-            .foregroundColor(.primary)
         }
+        .id(1)
         .listRowSeparator(.hidden)
-      }
-      if alsoFromVM.isLoading {
-        ProgressView()
-          .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+        if alsoFromVM.characters.count > 0 {
+          Section {
+            ForEach(alsoFromVM.characters, id: \.id) { item in
+              Button {
+                swapSelectedCharacter(with: item)
+                withAnimation {
+                  proxy.scrollTo(1, anchor: .top)
+                }
+              } label: {
+                CharacterCardView(character: item)
+              }
+            }
+          } header: {
+            Text("Also from \"\(selectedCharacter.location.name)\"")
+              .font(.title)
+              .bold()
+              .foregroundColor(.primary)
+          }
           .listRowSeparator(.hidden)
+        }
+        if alsoFromVM.isLoading {
+          ProgressView()
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            .listRowSeparator(.hidden)
+        }
       }
+      .environment(\.defaultMinListRowHeight, 120)
+      .listStyle(.inset)
     }
-    .environment(\.defaultMinListRowHeight, 120)
-    .listStyle(.inset)
     .navigationTitle(selectedCharacter.name)
     .onAppear {
       alsoFromVM.fetchLocation(for: selectedCharacter)
